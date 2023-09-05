a96dd2949c7a20df1a131341dafcc993c3a6bb14
Niranjan Salunkhe <101391587+nsalunkhe@users.noreply.github.com>
2023-09-05 23:43:16 +0530

    added

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
-
-
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
-var xhub = require('express-x-hub');
 
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
-
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
 var token = process.env.TOKEN || 'token';
+var secret = process.env.APP_SECRET || 'secret';
 var received_updates = [];
 
-app.get('/', function(req, res) {
-  console.log(req);
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (req, res) {
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
-app.get('/facebook', function(req, res) {
+app.get('/facebook', function (req, res) {
   if (
     req.query['hub.mode'] == 'subscribe' &&
     req.query['hub.verify_token'] == token
@@ -30,19 +25,22 @@ app.get('/facebook', function(req, res) {
   }
 });
 
-app.post('/facebook', function(req, res) {
+app.post('/facebook', function (req, res) {
   console.log('Facebook request body:', req.body);
 
-  if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
+  // You can check the secret if needed
+  if (req.headers['x-hub-signature'] !== secret) {
+    console.log('Warning - request header X-Hub-Signature not valid');
     res.sendStatus(401);
     return;
   }
 
-  console.log('request header X-Hub-Signature validated');
+  console.log('Request header X-Hub-Signature validated');
   // Process the Facebook updates here
   received_updates.unshift(req.body);
   res.sendStatus(200);
 });
 
-app.listen();
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
